97061d45de4908361f522d991653e986517d2d0c
Mohamad_Hawchar <mohamad_hawchar@hotmail.com>
2022-10-31 22:24:38 -0400

    Changed some bugs

--- src/train_segmentation.py
+++ src/train_segmentation.py
@@ -473,7 +473,7 @@ def my_app(cfg: DictConfig) -> None:
             gpu_args.pop("val_check_interval")
 
     else:
-        gpu_args = dict(gpus=-1, accelerator='ddp', val_check_interval=cfg.val_freq)
+        gpu_args = dict(gpus=-1, accelerator='cuda', val_check_interval=cfg.val_freq)
         # gpu_args = dict(gpus=1, accelerator='ddp', val_check_interval=cfg.val_freq)
 
         if gpu_args["val_check_interval"] > len(train_loader) // 4:

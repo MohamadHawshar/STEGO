cfdb0dbf0538fabbbc22a60135eff1c73718b65a
Mohamad Hawchar <60289580+MohamadHawshar@users.noreply.github.com>
2023-08-19 14:01:00 -0400

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -11,7 +11,7 @@ import torch.nn.functional as F
 import wget
 from PIL import Image
 from scipy.optimize import linear_sum_assignment
-from torch._six import string_classes
+#from torch._six import string_classes
 from torch.utils.data import DataLoader
 from torch.utils.data._utils.collate import np_str_obj_array_pattern, default_collate_err_msg_format
 from torchmetrics import Metric
